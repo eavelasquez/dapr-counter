d23d96e8bfb3f59975020dfae9bed87b283cda72
Esteban Velasquez <eavc.30@gmail.com>
2022-02-16 19:29:30 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using Dapr.Client;
+
+const string storeName = "statestore";
+const string keyCounter = "counter";
+const string keyAccumulator = "accumulator";
+
+
+var daprClient = new DaprClientBuilder().Build();
+var counter = await daprClient.GetStateAsync<int>(storeName, keyCounter);
+var accumulator = await daprClient.GetStateAsync<int>(storeName, keyAccumulator);
+
+while (true)
+{
+  Console.WriteLine($"Counter = {counter++}");
+  Console.WriteLine($"Accumulator = {accumulator--}");
+
+  await daprClient.SaveStateAsync(storeName, keyCounter, counter);
+  await daprClient.SaveStateAsync(storeName, keyAccumulator, accumulator);
+  await Task.Delay(1000);
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Darp Counter
+
+Run the application with the following command:
+
+```bash
+dapr run --app-id DaprCounter dotnet run
+```
